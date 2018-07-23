a823f92be24b163896186b565dcab8a4a478331f
Minka5 <madrakowsky5@gmail.com>
2018-07-23 20:11:31 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,23 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
+* guest_name
+* party_name
+* date
+* time
+* host_name
+
+...and then prints out custom invitations that look something like this:
+
+```
+Dear Hermione,
+
+You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30.
+
+Sincerely,
+
+Harry Potter
+```
+
+puts "What is your name?"
+guest_name = gets.chomp
